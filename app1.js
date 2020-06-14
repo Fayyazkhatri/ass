9714fd45120752b0b3cf50633721449af437614b
Fayyazkhatri <fayyazkhatri3@gmail.com>
2020-06-14 19:07:05 +0500

    now ready to commit

---
+++ app1.js
@@ -0,0 +1,168 @@
+// var thanks = "Please enter and valid password"
+
+// alert (thanks)
+
+// alert ("Welcome to JS Land.. \nHappy Coding!")
+
+// alert ("Welcome to JS Land")
+
+// alert ("Happy coding! \nPrevent this page from creating additional dialogs.")
+
+// var username
+
+// username = ("Fayyaz Ahmed")
+
+// var message = "Hello World"
+
+// alert (message)
+
+// var name = "Fayyaz"
+
+// var age = "30"
+
+// var grade = "certified mobile application development"
+
+// alert (name)
+// alert (age)
+// alert (grade)
+
+// alert (" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// var email = " fayyazkhatri3@gmail.com";
+// var text = "My email id is"
+// var message = text + email
+// alert (message)
+
+// var book 
+
+// book = "A smater way to learn from the book"
+
+// alert ("I am trying to learn from the book "  + book )
+
+// var HTML = "Yah! I can write content through javaScript"
+
+// document.write(HTML)
+
+
+// var HTML = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(HTML)
+
+// var age = 15
+
+// alert("i am " + age + " years old")
+
+
+// var N = 14
+
+// alert("You have visited this site " + N + " times   ")
+
+// var birthYear = 1991 ;
+
+// document.write ("My Birth year is " + birthYear + "<br/> Data Type of my decleard variable is number")
+
+
+// document.write("<b>John Doe</b> ordered <b> 5 T-shirt</b>(s) on XYZ Clothing store")
+
+// var x = 1 , y = 2 , z = 3 ;
+
+// var message
+// var message2
+// var message3
+// var massage4 
+
+// message = "variable names can only contain , 300, $, and _ for example :$my_1st variable" 
+// message2 = "variables must begin with a six hundred, $ or _ for example: $name,_ name or name "
+// message3 = "variable names are case sensitive"
+// message4 = "variable names should not be JS keywords" ;
+
+// document.write("<h1>Rules for nameing JS variable</h1>")
+
+// document.write (message + "<br/>" + message2  + "<br/>" + message3 + "<br/>" + message4)
+
+
+// document.write("Sum of 3 and 5 is " , 3 + 5 ) 
+
+
+// document.write("Sum of 3 and 5 is " , 3 - 5 ) 
+// document.write("Sum of 3 and 5 is " , 3 * 5 ) 
+// document.write("Sum of 3 and 5 is " , 3 / 5 )  
+// document.write("Sum of 3 and 5 is " , 3 % 5 ) 
+
+var b ;
+// var a = 5
+// var c = ++a + 7 + --a - ++a / 3 
+// alert(c) 
+
+
+
+
+
+
+// document.write("Value after variable declaration is: " + b + "<br/> Initial value: 5 <br/> Value after increment is: 6 <br/> Value after addition is: 13 <br/> Value after decrement is: 12")  
+
+// document.write("Total cost to buy 5 tickets to a movie is ", 5 * 600 ,"PKR")
+
+
+// for(var i = 1 ; i <= 10 ; i++)
+
+// document.write( "4" + "x" + i + "=" , 4*i + "<br>")
+
+// var item1 = 650*3
+// var item2 = 100*7
+// var shipping = 100
+// var total = item1 + item2 + shipping
+
+// document.write("Total cost of products is " + total)
+  
+ 
+// var total = 980
+// var obtain = 804
+// var percentage = obtain / total * 100
+
+// document.write("percentage : " + percentage )
+
+// var us = 104.8 * 10
+// var sr = 28 * 25
+
+// var total = us + sr 
+
+// document.write("Total Curreny in PKR:" + total)
+
+// var a = (1 + 5) * 10 / 2  
+
+// document.write(a)
+
+// var current = 2020
+// var birth = 1991
+// var age = current - birth
+
+// document.write(age)
+
+// var radius = 20
+// var circum = 20 * 2 * 3.142
+// var area = 20 * 20 * 3.142
+
+// document.write("Radius of a circle is: " + radius + "<br> The circumference is: " + circum + "<br> The are is: " + area)
+
+// var current = 29
+// var max = 80
+// var estimate = 2
+
+// var total = (max - current) * estimate   
+
+// document.write("You will need " + total + " Chocolate to last you until the ripe old age of " + max) 
+
+// var a = 10
+// var b = ++a
+// var c = b + a++
+
+// document.write("Result <br> Total value of a is:" + a);
+// document.write("<br>")
+// document.write(" <br> ************************")
+// document.write("<br>")
+// document.write(" <br>The value of ++a is:" + b )
+// document.write("<br>")
+// document.write("Now the value of a is:" + b)
+
+document.write(" <h1> Dear sir me Fayyaz Ahmed ( rol number # WM3520) and my wife (Komal Fayyaz Rol # WM4924 ) using same laptop, Due to some technical issue in laptop we both not able to do practice of javaScript please allow some time to both of us we will take clases again and do paractice.  </h1> ")
